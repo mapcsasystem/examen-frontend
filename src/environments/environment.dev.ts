859eca69575acc65d6af960a13808d77ff3eeee8
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-01 00:19:14 -0600

    add environments dev

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  baseUrlApi: 'http://52.146.19.160:9443/api-listas-internas/',
+  dev: 'dev',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   baseUrlApi: 'http://52.146.19.160:9443/api-listas-internas/',
-  dev: 'dev',
+  dev: 'development',
 };
 
 /*
